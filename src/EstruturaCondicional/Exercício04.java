88e8508416d279e48a40997811b4a35cada5a313
Juliano S Orlandi <js.orlandi.dev@gmail.com>
2021-02-21 12:08:50 -0300

    add exercício04

--- src/EstruturaCondicional/Exercício04.java
+++ src/EstruturaCondicional/Exercício04.java
@@ -1,10 +1,28 @@
 package EstruturaCondicional;
 
+import java.util.Scanner;
+
 public class Exercício04 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int horaInicial, horaFinal, duracao;
+		
+		horaInicial = sc.nextInt();
+		horaFinal = sc.nextInt();
+		
+		if(horaInicial < horaFinal) {
+			duracao = horaFinal - horaInicial;
+		}
+		else {
+			duracao = 24 - horaInicial + horaFinal;
+		}
+		
+		System.out.printf("O JOGO DUROU %d HORA(S)", duracao);
+		
+		sc.close();
 	}
 
 }
